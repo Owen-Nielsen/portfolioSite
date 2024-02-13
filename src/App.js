c21a2a5b19cf7f3036d89d9640c7b4aa20960196
Owen Nielsen <owennielsen25@amdg.rockhursths.edu>
2024-02-13 13:33:46 -0600

    set up new repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top-bar">
+        This is the top bar
+        <button className="top-bar-button">Click me</button>
+      </div>
+      {/* Rest of your app goes here */}
+    </div>
+  );
+}
+
+export default App;
